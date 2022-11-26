dd5c24c4ca6c293953056c92449937cfa15576f9
Lance Delos Reyes <lan_drey19@hotmail.com>
2022-11-26 17:19:11 +1300

    filter based on genre

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const express = require("express");
+const items = require("./data.json");
 
 const app = express();
 
 app.get("/search", (req, res) => {
   if (!req.query || !req.query.term.trim()) {
-    res.send("return all");
+    res.send(items);
   } else {
-    res.send("filtered result");
+    const filteredData = items.filter(
+      (item) =>
+        item.genre.toLowerCase().trim() === req.query.term.toLowerCase().trim()
+    );
+    res.send(filteredData);
   }
 });
 
