fa92fb761c349b38727e0c60402c0c5943cb2342
Lance Delos Reyes <lan_drey19@hotmail.com>
2022-11-27 18:56:20 +1300

    add exclusion filter if substring

--- app.js
+++ app.js
@@ -28,66 +28,124 @@ app.get("/search", (req, res) => {
     if (
       Object.keys(req.query).length > 1 &&
       "exclude" in req.query &&
-      "include" in req.query
+      "include" in req.query &&
+      "has" in req.query &&
+      "hasnot" in req.query
     ) {
       res
         .status(400)
         .json({
           err_msg:
-            "you can not have both include and exclude keys in your query parameters. However, you can have multiple instances of only one of them",
+            "you can not have both inclusion-related and exclusion-related keys in your query parameters However, you can have multiple instances of only one of them i.e. mulitiple instances of include and has or multiple instances of exclude and hasnot",
         })
         .end();
     }
 
-    if ("include" in req.query) {
-      let searchTermsIncludeArray = [];
-      if (!Array.isArray(req.query.include)) {
-        searchTermsIncludeArray.push(req.query.include);
-      } else {
-        searchTermsIncludeArray = req.query.include;
+    let searchTermsIncludeArray = [];
+    let searchTermsHasArray = [];
+    let searchTermsExcludeArray = [];
+    let searchTermsHasNotArray = [];
+
+    if ("include" in req.query || "has" in req.query) {
+      let searchTermsIncludeArrayLowerCase = [];
+      let searchTermsHasArrayLowerCase = [];
+      if ("include" in req.query) {
+        if (!Array.isArray(req.query.include)) {
+          searchTermsIncludeArray.push(req.query.include);
+        } else {
+          searchTermsIncludeArray = req.query.include;
+        }
+        searchTermsIncludeArrayLowerCase = searchTermsIncludeArray.map((item) =>
+          item.toLowerCase().trim()
+        );
       }
-      const searchTermsIncludeArrayLowerCase = searchTermsIncludeArray.map(
-        (item) => item.toLowerCase().trim()
-      );
+
+      if ("has" in req.query) {
+        if (!Array.isArray(req.query.has)) {
+          searchTermsHasArray.push(req.query.has);
+        } else {
+          searchTermsHasArray = req.query.has;
+        }
+        searchTermsHasArrayLowerCase = searchTermsHasArray.map((item) =>
+          item.toLowerCase().trim()
+        );
+      }
 
       finalResults = currentItems.filter((item) => {
-        console.log(item.genre.toLowerCase().trim());
         if (
           searchTermsIncludeArrayLowerCase.includes(
             item.genre.toLowerCase().trim()
-          )
+          ) ||
+          searchTermsHasArrayLowerCase.find((hasTerm) => {
+            if (
+              item.genre
+                .toLowerCase()
+                .trim()
+                .includes(hasTerm.toLowerCase().trim())
+            ) {
+              return true;
+            }
+          })
         ) {
           return item;
         }
       });
 
       res.status(200).json(finalResults).end();
-    } else if ("exclude" in req.query) {
-      let searchTermsExcludeArray = [];
-      if (!Array.isArray(req.query.exclude)) {
-        searchTermsExcludeArray.push(req.query.exclude);
-      } else {
-        searchTermsExcludeArray = req.query.exclude;
+    } else if ("exclude" in req.query || "hasnot" in req.query) {
+      let searchTermsExcludeArrayLowerCase = [];
+      let searchTermsHasNotArrayLowerCase = [];
+      if ("exclude" in req.query) {
+        if (!Array.isArray(req.query.exclude)) {
+          searchTermsExcludeArray.push(req.query.exclude);
+        } else {
+          searchTermsExcludeArray = req.query.exclude;
+        }
+        searchTermsExcludeArrayLowerCase = searchTermsExcludeArray.map((item) =>
+          item.toLowerCase().trim()
+        );
       }
-      const searchTermsExcludeArrayLowerCase = searchTermsExcludeArray.map(
-        (item) => item.toLowerCase().trim()
-      );
+
+      if ("hasnot" in req.query) {
+        if (!Array.isArray(req.query.hasnot)) {
+          searchTermsHasNotArray.push(req.query.hasnot);
+        } else {
+          searchTermsHasNotArray = req.query.hasnot;
+        }
+        searchTermsHasNotArrayLowerCase = searchTermsHasNotArray.map((item) =>
+          item.toLowerCase().trim()
+        );
+      }
+
       finalResults = currentItems.filter((item) => {
         if (
-          !searchTermsExcludeArrayLowerCase.includes(
-            item.genre.toLowerCase().trim()
+          !(
+            searchTermsExcludeArrayLowerCase.includes(
+              item.genre.toLowerCase().trim()
+            ) ||
+            searchTermsHasNotArrayLowerCase.find((hasTerm) => {
+              if (
+                item.genre
+                  .toLowerCase()
+                  .trim()
+                  .includes(hasTerm.toLowerCase().trim())
+              ) {
+                return true;
+              }
+            })
           )
         ) {
           return item;
         }
       });
+
       res.status(200).json(finalResults).end();
     }
   } catch (err) {
     //   res.status(400)
     //   .json({
     //     error_message:
-    //       "you must enter a valid query term in this format e.g. http://localhost:3000/search?term=<your_search_term_here> or http://localhost:3000/search?term=<your_first_search_term_here>&term=<your_second_search_term_here>",
+    //       "you must enter a valid query term in this format e.g. http://localhost:3000/search?include=<your_search_term_here> or http://localhost:3000/search?exclude=<your_first_search_term_here>&exclude=<your_second_search_term_here>",
     //   })
     //   .end();
 
