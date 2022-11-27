983b4f8d363fbdbcb5ae1bead03ba681c853b02a
Lance Delos Reyes <lan_drey19@hotmail.com>
2022-11-27 14:03:54 +1300

    add exclusion and only either include or exclude allowed

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const app = express();
 
 app.get("/search", (req, res) => {
   try {
+    let finalResults;
     const noResults = [];
     if (
       !Array.isArray(req.query["include"]) &&
@@ -12,7 +13,7 @@ app.get("/search", (req, res) => {
       "include" in req.query
     ) {
       if (req.query.include.trim() === "") {
-        res.status(200).json(currentItems);
+        res.status(200).json(currentItems).end();
       }
     } else if (
       !Array.isArray(req.query["exclude"]) &&
@@ -20,13 +21,26 @@ app.get("/search", (req, res) => {
       "exclude" in req.query
     ) {
       if (req.query.exclude.trim() === "") {
-        res.status(200).json(noResults);
+        res.status(200).json(noResults).end();
       }
     }
 
-    let searchTermsIncludeArray = [];
+    if (
+      Object.keys(req.query).length > 1 &&
+      "exclude" in req.query &&
+      "include" in req.query
+    ) {
+      res
+        .status(400)
+        .json({
+          err_msg:
+            "you can not have both include and exclude keys in your query parameters. However, you can have multiple instances of only one of them",
+        })
+        .end();
+    }
 
     if ("include" in req.query) {
+      let searchTermsIncludeArray = [];
       if (!Array.isArray(req.query.include)) {
         searchTermsIncludeArray.push(req.query.include);
       } else {
@@ -35,8 +49,8 @@ app.get("/search", (req, res) => {
       const searchTermsIncludeArrayLowerCase = searchTermsIncludeArray.map(
         (item) => item.toLowerCase().trim()
       );
-      console.log(searchTermsIncludeArrayLowerCase);
-      const filteredItems = currentItems.filter((item) => {
+
+      finalResults = currentItems.filter((item) => {
         console.log(item.genre.toLowerCase().trim());
         if (
           searchTermsIncludeArrayLowerCase.includes(
@@ -46,11 +60,28 @@ app.get("/search", (req, res) => {
           return item;
         }
       });
-      res.status(200).json(filteredItems);
-    }
 
-    if ("exclude" in req.query) {
+      res.status(200).json(finalResults).end();
+    } else if ("exclude" in req.query) {
       let searchTermsExcludeArray = [];
+      if (!Array.isArray(req.query.exclude)) {
+        searchTermsExcludeArray.push(req.query.exclude);
+      } else {
+        searchTermsExcludeArray = req.query.exclude;
+      }
+      const searchTermsExcludeArrayLowerCase = searchTermsExcludeArray.map(
+        (item) => item.toLowerCase().trim()
+      );
+      finalResults = currentItems.filter((item) => {
+        if (
+          !searchTermsExcludeArrayLowerCase.includes(
+            item.genre.toLowerCase().trim()
+          )
+        ) {
+          return item;
+        }
+      });
+      res.status(200).json(finalResults).end();
     }
   } catch (err) {
     //   res.status(400)
